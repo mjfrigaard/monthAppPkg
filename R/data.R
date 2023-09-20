666f01d0c46f6d9a1641c752883f8efe4c48b827
Martin Frigaard <mjfrigaard@pm.me>
2023-09-20 02:03:14 -0500

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Birthstones data 
+#'
+#' A dataset containing the month and associated birthstone. The variables are
+#'  as follows:
+#'
+#' @format A data frame with 12 rows and 2 variables:
+#' \describe{
+#'   \item{month}{name of month (January--December)}
+#'   \item{stone}{name of stone}
+#' }
+"stones"
